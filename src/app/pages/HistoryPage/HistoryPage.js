6d2407ff666baf9c9302b1ffef423065021054af
Duc <nguyenthanhduc242004@gmail.com>
2025-07-17 06:47:28 +0700

    Show message when user haven't viewed any products.

--- src/app/pages/HistoryPage/HistoryPage.js
+++ src/app/pages/HistoryPage/HistoryPage.js
@@ -13,17 +13,21 @@ export default function HistoryPage() {
     return (
         <>
             <h2 className="text-3xl font-bold text-gray-800 mb-6">Lịch sử xem gần đây</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-4">
-                {viewedProducts.map((product) => (
-                    <ProductCard
-                        key={product.id}
-                        product={product}
-                        onDetailClick={handleProductDetailClick}
-                        onToggleFavorite={handleToggleFavorite}
-                        isFavorite={isProductFavorite(product.id)} // Always true on this page
-                    />
-                ))}
-            </div>
+            {viewedProducts.length === 0 ? (
+                <p className="text-gray-600 text-lg text-center py-10">Bạn chưa xem khóa học nào.</p>
+            ) : (
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-4">
+                    {viewedProducts.map((product) => (
+                        <ProductCard
+                            key={product.id}
+                            product={product}
+                            onDetailClick={handleProductDetailClick}
+                            onToggleFavorite={handleToggleFavorite}
+                            isFavorite={isProductFavorite(product.id)} // Always true on this page
+                        />
+                    ))}
+                </div>
+            )}
         </>
     );
 }
