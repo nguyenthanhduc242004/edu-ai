6f586d6021d8fdafdd929d204fbc621ecfa9ce61
Duc <nguyenthanhduc242004@gmail.com>
2025-07-17 06:04:49 +0700

    Vercel fix test 2.

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-        './Components/**/*.{js,ts,jsx,tsx}',
-        './src/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
-};
